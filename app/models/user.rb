2c3ac0ac39005ccc63c68452647f230686a5136a
Vinay Chand <vinaychandvl@gmail.com>
2013-02-27 08:43:06 -0500

    Add/Edit User enabled

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :emailID, :firstName, :lastName, :userName, :password, :roles
-  validates :emailID,  :presence => true
+  validates :emailID,   :presence => true
   validates :firstName, :presence => true
   validates :lastName,  :presence => true
-  validates :userName, :presence => true
+  validates :userName,  :presence => true
   validates :password,  :presence => true
 end
