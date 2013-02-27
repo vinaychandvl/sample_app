c71ed3e3ba7b9723d42602ad19888584583d4e8e
Vinay Chand <vinaychandvl@gmail.com>
2013-02-27 10:32:07 -0500

    added multi column filter

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
   end
+  
+  def login
+    
+  end
+  
+  def signup
+    
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   validates :lastName,  :presence => true
   validates :userName,  :presence => true
   validates :password,  :presence => true
+  validates_uniqueness_of :userName, :emailID
+  validates_length_of :userName, :within => 3..40
+  validates_length_of :password, :within => 5..40
 end
